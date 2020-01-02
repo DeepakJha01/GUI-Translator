356382b7a835965f4ef51ca946865ab7041d89b3
Deepak Jha <45568398+DeepakJha01@users.noreply.github.com>
2020-01-02 20:21:31 +0530

    comments added

--- main/gui_translator.py
+++ main/gui_translator.py
@@ -1,19 +1,20 @@
 from tkinter import *
-from textblob import TextBlob
-from tkinter.scrolledtext import ScrolledText
+from textblob import TextBlob                   ##language translation API
+from tkinter.scrolledtext import ScrolledText   ##for scrollable text box
+
 def toHindi():
     try:
-        output.delete("1.0",END)
+        output.delete("1.0",END)                ##to delete previous entry in the output box
         inputSTR = input_str.get("1.0",END)
         obj = TextBlob(str(inputSTR))
         outputSTR = obj.translate(to='hi')
-        output.insert(END,str(outputSTR))
+        output.insert(END,str(outputSTR))       ##insert output to the output box
     except:
         pass
 
 root = Tk()
 root.title("Instant Translator")
-root.geometry("360x400+400+200")
+root.geometry("360x400+400+200")                ##width x height + xpos + ypos
 root.configure(bg = "orange")
 
 label1 = Label(root,text = "Translate to Hindi", fg="white",bg="black", font = ("Times New Roman","18"))
@@ -21,13 +22,13 @@ label1.place(x=90,y=30)
 
 label2 = Label(root,text = "Type Anything:",fg="blue",font = ("Times New Roman","12"))
 label2.place(x=20,y=90)
-
+                                                ##scrollable text box with multiple lines of input
 input_str = ScrolledText(root,width=38,height=5,font=("Times New Roman","12"))
 input_str.place(x=20,y=120)
-
+                                                ##button binded to toHindi() function
 translate_button = Button(root,text = "Translate",command = toHindi,fg="black",bg="red")
 translate_button.place(x=150,y=232)
-
+                                                ##scrollable text box with multiple lines of output
 output = ScrolledText(root,width = 38,height=5,font=("Times New Roman","12"))
 output.place(x=20,y=270)
 
