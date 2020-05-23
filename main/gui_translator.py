f77709cb81448a039758cfa0e94d045b9f4019e2
Deepak Jha <45568398+DeepakJha01@users.noreply.github.com>
2020-05-23 09:37:36 +0530

    reduced redundant codes

--- main/gui_translator.py
+++ main/gui_translator.py
@@ -2,46 +2,19 @@ from tkinter import *
 from textblob import TextBlob                   ##language translation API
 from tkinter.scrolledtext import ScrolledText   ##for scrollable text box
 
-def toHindi():
+#-----translation function----------------------------------------------------------------------
+def toLang(lang):
     try:
         output.delete("1.0",END)            ##to delete previous entry in the output box
         inputSTR = input_str.get("1.0",END)
         obj = TextBlob(str(inputSTR))
-        outputSTR = obj.translate(to='hi')
-        output.insert(END,str(outputSTR))   ##insert output to the output box
-    except:
-        output.insert(END,"**Please enter a meaningful word/sentence**")
-
-def toFrench():
-    try:
-        output.delete("1.0",END)            ##to delete previous entry in the output box
-        inputSTR = input_str.get("1.0",END)
-        obj = TextBlob(str(inputSTR))
-        outputSTR = obj.translate(to='fr')
+        outputSTR = obj.translate(to=lang)
         output.insert(END,str(outputSTR))   ##insert output to the output box
     except:
         output.insert(END, "**Please enter a meaningful word/sentence**")
 
-def toSpanish():
-    try:
-        output.delete("1.0",END)            ##to delete previous entry in the output box
-        inputSTR = input_str.get("1.0",END)
-        obj = TextBlob(str(inputSTR))
-        outputSTR = obj.translate(to='es')
-        output.insert(END,str(outputSTR))   ##insert output to the output box
-    except:
-        output.insert(END, "**Please enter a meaningful word/sentence**")
-
-def toRussian():
-    try:
-        output.delete("1.0",END)            ##to delete previous entry in the output box
-        inputSTR = input_str.get("1.0",END)
-        obj = TextBlob(str(inputSTR))
-        outputSTR = obj.translate(to='ru')
-        output.insert(END,str(outputSTR))   ##insert output to the output box
-    except:
-        output.insert(END, "**Please enter a meaningful word/sentence**")
-
+        
+#-----user interface section---------------------------------------------------------------------
 root = Tk()
 root.title("Instant Translator")
 root.geometry("400x500+400+200")            ##width x height + xpos + ypos
@@ -59,16 +32,16 @@ input_str.place(x=20,y=120)
 label3 = Label(root,text = "Translate to :",fg="black",bg="yellow",font = ("Times New Roman","13"))
 label3.place(x=150,y=270)
 
-translate_button = Button(root,text = "HINDI",command = toHindi,fg="white",bg="green")
+translate_button = Button(root,text = "HINDI",command = toLang('hi'),fg="white",bg="green")
 translate_button.place(x=75,y=300)
 
-translate_button = Button(root,text = "FRENCH",command = toFrench,fg="white",bg="blue")
+translate_button = Button(root,text = "FRENCH",command = toLang(''fr'),fg="white",bg="blue")
 translate_button.place(x=130,y=300)
 
-translate_button = Button(root,text = "SPANISH",command = toSpanish,fg="white",bg="#B80000")
+translate_button = Button(root,text = "SPANISH",command = toLang('es'),fg="white",bg="#B80000")
 translate_button.place(x=200,y=300)
 
-translate_button = Button(root,text = "RUSSIAN",command = toRussian,fg="white",bg="#330099")
+translate_button = Button(root,text = "RUSSIAN",command = toLang('ru'),fg="white",bg="#330099")
 translate_button.place(x=270,y=300)
                                             ##scrollable text box with multiple lines of output
 output = ScrolledText(root,width = 42,height=6,font=("Times New Roman","12"))
